cc20c4ddd5362f626d7d2816e3e427baab7287b7
unknown <vardhansparsh46@gmail.com>
2026-04-03 14:24:11 +0530

    added GET rotutes for /records and /dashboard/summary

--- src/routes/recordRoutes.js
+++ src/routes/recordRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { createRecord } = require("../controllers/recordController");
+const { createRecord, getRecords, getSummary } = require("../controllers/recordController");
 const authorizeAdmin = require("../middleware/authMiddleware");
 const validateRecord = require("../middleware/validateRecord");
 const router = express.Router();
 
 router.post("/records", validateRecord, authorizeAdmin, createRecord);
+
+router.get("/records", getRecords)
+
+router.get("/dashboard/summary", getSummary);
 
 module.exports = router;
